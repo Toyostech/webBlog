fe6e802fb26ea99f88ff20550c0037f79720fdac
toyosi <salamitoyosikabire.com>
2025-03-01 03:38:14 +0100

    workking on the service page

--- src/Component/abouts/Abouts.jsx
+++ src/Component/abouts/Abouts.jsx
@@ -22,7 +22,7 @@ const Abouts = () => {
           alignItems: "center",
           textAlign: "center",
 
-          padding: "0px 40px",
+          padding: "0px 20px",
           backgroundColor: "rgba(0, 0, 0, 0.3)",
           width: "100%",
           height: "100%"
